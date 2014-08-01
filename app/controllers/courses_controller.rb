1a1e76e6d5392d6fb587fdd2ae0931b7e3ca6c98
Anil Kumar Alluri <alluri.anil190@gmail.com>
2014-08-01 12:43:47 +0530

    Update courses_controller.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,11 @@
 class CoursesController < ApplicationController
-  
-  #layout "courses"
-
+    #layout "courses"
   def index
     @courses = Course.all
   end
-  
   def new
     @course = Course.new
   end
-  
   def create
     @course = Course.new
     # render :text => params.inspect and return
@@ -25,23 +21,19 @@ class CoursesController < ApplicationController
       render 'new'
     end
   end
-
   def show
     @course = Course.find(params[:id])
   end
-
   def edit
     @course = Course.find(params[:id])
     #render :text => params.inspect and return
   end
   def update
-
     @course = Course.find(params[:id])
     @course.title = params[:course][:title]
     @course.outcome = params[:course][:outcome]
     @course.description = params[:course][:description]
     @course.fee = params[:course][:fee]
-    
     if @course.save
       redirect_to courses_path
     else
@@ -53,6 +45,4 @@ class CoursesController < ApplicationController
       @course = Course.find(params[:id]).destroy
       redirect_to courses_path
   end
-
-
 end
