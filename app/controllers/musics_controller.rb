a461e8bf0a6c13e8edd8bc1e6d89fdb5e23aeea3
Anil Kumar Alluri <alluri.anil190@gmail.com>
2014-08-01 12:45:51 +0530

    Update musics_controller.rb

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,31 +1,25 @@
 class MusicsController < ApplicationController
   before_action :set_music, only: [:show, :edit, :update, :destroy]
-
   # GET /musics
   # GET /musics.json
   def index
     @musics = Music.all
   end
-
   # GET /musics/1
   # GET /musics/1.json
   def show
   end
-
   # GET /musics/new
   def new
     @music = Music.new
   end
-
   # GET /musics/1/edit
   def edit
   end
-
   # POST /musics
   # POST /musics.json
   def create
     @music = Music.new(music_params)
-
     respond_to do |format|
       if @music.save
         format.html { redirect_to @music, notice: 'Music was successfully created.' }
@@ -36,7 +30,6 @@ class MusicsController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /musics/1
   # PATCH/PUT /musics/1.json
   def update
@@ -50,7 +43,6 @@ class MusicsController < ApplicationController
       end
     end
   end
-
   # DELETE /musics/1
   # DELETE /musics/1.json
   def destroy
@@ -60,13 +52,11 @@ class MusicsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_music
       @music = Music.find(params[:id])
     end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def music_params
       params.require(:music).permit(:image, :document, :audio)
