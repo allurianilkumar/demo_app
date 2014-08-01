0649f0ec3f1247dee6adce19c309cb0042ab9f74
Anil Kumar Alluri <alluri.anil190@gmail.com>
2014-08-01 12:49:26 +0530

    Update gadget.rb

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,16 +1,9 @@
 class Gadget < ActiveRecord::Base
-  
   has_and_belongs_to_many :analysis_groups 
   has_and_belongs_to_many :type_of_methods
   belongs_to :currencys
   has_and_belongs_to_many :qualify_area_of_interests
-
 end
-
-
 # class Currency < ActiveRecord::Base
-  
 #   has_one :gadgets
-  
 # end
-
