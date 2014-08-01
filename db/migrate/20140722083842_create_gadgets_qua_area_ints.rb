a891a7bccfbb993eb2b3e5cd7c8970a25093b69b
Anil Kumar Alluri <alluri.anil190@gmail.com>
2014-08-01 13:20:56 +0530

    Update 20140722083842_create_gadgets_qua_area_ints.rb

--- db/migrate/20140722083842_create_gadgets_qua_area_ints.rb
+++ db/migrate/20140722083842_create_gadgets_qua_area_ints.rb
@@ -1,6 +1,5 @@
 class CreateGadgetsQuaAreaInts < ActiveRecord::Migration
-  
-  def up
+    def up
     create_table :gadgets_qua_area_ints do |t|
       t.references :qualify_area_of_intrests, index: true
       t.references :gadgets, index: true
