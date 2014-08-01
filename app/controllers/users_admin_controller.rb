0aabf795445407090e980d1ee53bbcd713833788
Anil Kumar Alluri <alluri.anil190@gmail.com>
2014-08-01 12:46:48 +0530

    Update users_admin_controller.rb

--- app/controllers/users_admin_controller.rb
+++ app/controllers/users_admin_controller.rb
@@ -1,14 +1,10 @@
 class UsersAdminController < ApplicationController
-
   def index
     @users = User.all
   end
-
   def show
     @user = User.find(params[:id])
-    
   end
-
   def create
     @user = User.new
     @user.first_name = params[:user][:first_name]
@@ -16,33 +12,26 @@ class UsersAdminController < ApplicationController
     @user.institution = params[:user][:institution]
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
-    
     if @user.save
       redirect_to users_admin_index_path
     else
       render 'new'
     end
   end
-  
   def new
     @user= User.new
   end
-  
   def edit
     @user = User.find(params[:id])
   end
-
   def update
-
     @user = User.find(params[:id])
-
     @user.first_name = params[:user][:first_name]
     @user.last_name = params[:user][:last_name]
     @user.institution = params[:user][:institution]
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.role = params[:user][:role]
-    
     if @user.save
       redirect_to users_admin_index_path
     else
@@ -50,10 +39,8 @@ class UsersAdminController < ApplicationController
       #flash[:notice] = "You are signed up successfully"
     end
   end
-
   def destroy
       User.find(params[:id]).destroy
       redirect_to users_admin_index_path
   end
-  
 end
